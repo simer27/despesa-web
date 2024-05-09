1b2236ed14742b8e53f40eae3c7ad7a51aa3c5a0
Simeao Almeida <simer27@yahoo.com>
2024-05-09 07:39:59 -0300

    componentes sidebar

---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+import icons from "../../styles/icons.js";
+
+
+const Sidebar = () => {
+  return <div className="sidebar">
+    <Link to="/"><img className="icon" src={icons.home} /></Link>
+    <Link to="/despesa"><img className="icon" src={icons.despesa} /></Link>
+    
+  </div>
+}
+
+export default Sidebar;
