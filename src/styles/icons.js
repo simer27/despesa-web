8ab2f1fee18f1e75c8fadb6b89b905f671795989
Simeao Almeida <simer27@yahoo.com>
2024-05-09 07:40:45 -0300

    icons Global para a aplicação

---
+++ src/styles/icons.js
@@ -0,0 +1,14 @@
+import casa from "../assets/casa.png";
+import home from "../assets/home.png";
+import despesa from "../assets/a-pagar.png";
+import logo from "../assets/logo.png";
+import editar from "../assets/edit.png";
+import deletar from "../assets/lixeira.png";
+import carro from "../assets/carro.png";
+import casa_familia from "../assets/casa-de-familia.png";
+import viagem from "../assets/viagem.png";
+import estudo from "../assets/estudos.png";
+import bicicleta from "../assets/bicicleta.png";
+import mercado from "../assets/mercado.png";
+
+export default {casa , home , despesa, logo, editar, deletar, carro, casa_familia, viagem, estudo, bicicleta, mercado };
